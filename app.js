2a085a2fbbe098358ddf99de90ee55b96d65aa78
Johnathan Brown <johnathaneb@gmail.com>
2016-08-12 15:32:04 -0400

    Added IP Address functionality

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 var express = require('express');
 var path = require("path");
+var useragent = require('express-useragent');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + "/index.html"), function(err) {
-      if (err) console.log(err);
-  });
+    res.sendFile(path.join(__dirname + "/index.html"), function(err) {
+        if (err) console.log(err);
+    });
 });
 
 app.get("/api/whoami", function(req, res) {
+    var ipAddr = request.headers['x-forwarded-for'];
+
     res.json({
-        Stuff: "Stuff"
+        ip: ipAddr
     });
-})
+});
 
 app.listen(process.env.PORT || 3000, function () {
-  console.log('App Started');
+    console.log('App Started');
 });
